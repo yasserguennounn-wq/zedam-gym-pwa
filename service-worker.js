ca2d12fc53b3233345be3a214a1c23bad0069811
yasserguennounn-wq <yasserguennounn@gmail.com>
2026-09-25 20:31:30 +0100

    Update cache version to zedam-gym-v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'zedam-gym-v1';
+const CACHE = 'zedam-gym-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
